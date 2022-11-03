f3e7b4f6291b9cfe3402d379e40867d7aa30b81a
Code Sandbox <hello@codesandbox.io>
2022-11-03 16:19:15 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7c79f3ffe6f96cbca40941845ebb4c42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b038d9c083ec2f6f70a343b60a13e1",
+    "url": "https://yasserzghinou.github.io/csb-h1827d/index.html"
+  },
+  {
+    "revision": "471d2cedf8b21c4dbeb4",
+    "url": "https://yasserzghinou.github.io/csb-h1827d/static/css/main.ff0d6d32.chunk.css"
+  },
+  {
+    "revision": "ddc6dc8fd2a814009e63",
+    "url": "https://yasserzghinou.github.io/csb-h1827d/static/js/2.fffe78a6.chunk.js"
+  },
+  {
+    "revision": "471d2cedf8b21c4dbeb4",
+    "url": "https://yasserzghinou.github.io/csb-h1827d/static/js/main.76fc7632.chunk.js"
+  },
+  {
+    "revision": "59295b19517f874a790b",
+    "url": "https://yasserzghinou.github.io/csb-h1827d/static/js/runtime~main.a744f449.js"
+  }
+]);
